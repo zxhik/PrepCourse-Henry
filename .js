4f7cbe3089dc4f76723b8893f32300a33b8b48fd
Lorenzo <zxhik000@gmail.com>
2024-08-14 00:20:08 -0300

    Patch 1.1

--- .js
+++ .js
@@ -1,1 +1,13 @@
-console.log (3+5);
+console.log ("Suma " + (3 + 5));
+console.log ("Resta " + (14 - 7));
+console.log ("Multiplicación " + (4 * 6));
+console.log ("División " + (30 / 9));
+console.log ("Resto " + (95 % 4));
+console.log ("Precedencia " + (3 +5 * 2 - 8));
+console.log ("Cambiando Precedencia " + ((3 +5) * 2 - 8));
+
+console.log ("Hola " + "Lorenzo");
+var nombre = "Lorenzo"
+var saludo = "Hola " + nombre;
+console.log (saludo);
+
