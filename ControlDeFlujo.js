c7481ad625030177fc8212fcb2545619b2f5a3e0
Lorenzo <zxhik000@gmail.com>
2024-08-15 23:35:05 -0300

    Patch 1.4

---
+++ ControlDeFlujo.js
@@ -0,0 +1,11 @@
+function viajar(destino) {
+    if(destino === 'Brasil') {
+             console.log('Gire a la izquierda');
+    } else if(destino === 'Argentina') {
+             console.log('Gire a la derecha');
+    } else {
+             console.log('Nos perdimos');''
+    }
+  };
+  viajar('Brasil');
+  viajar('Argentina');
