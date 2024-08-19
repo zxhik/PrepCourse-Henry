d82e1cd2003d22a69463b1583570ada1745b5fa1
Lorenzo <zxhik000@gmail.com>
2024-08-18 23:50:02 -0300

    Patch 1.5

---
+++ Bucles.js
@@ -0,0 +1,5 @@
+var suma = 0;
+for (var i=0; i < 10; i++){
+    suma = suma + 1;
+    console.log(suma);
+}
---
+++ operadoresLogicos.js
@@ -0,0 +1,25 @@
+//AND
+function mayorYMenorYPar(num) {
+	if( num > 5 && num < 10  && num  % 2 === 0 ) console.log(true);
+	else console.log(false);
+};
+mayorYMenorYPar(7);
+mayorYMenorYPar(8); 
+
+// OR
+function operadorOr(str) {
+    if (str === 'Henry' || str.length < 2) console.log(true);
+    else console.log(false);
+ }
+ operadorOr('Henry');
+ operadorOr('Javascript');
+ operadorOr('H');
+
+ 
+ // NOT
+function negacion(permiso) {
+    if (permiso) console.log('Tiene permiso');
+ }
+ negacion(true);
+ negacion(false);
+  
