dd0c6896f78b8ef1c16b1ef2904f6d73c1b12ec9
Lorenzo <zxhik000@gmail.com>
2024-08-20 23:37:50 -0300

    Patch 1.6

--- Bucles.js
+++ Bucles.js
@@ -1,5 +1,13 @@
 var suma = 0;
-for (var i=0; i < 10; i++){
+for (var i=0; i < 5; i++){
     suma = suma + 1;
+    console.log("Variable de iteracion ", i);
+}
+
+console.log("Variable suma ", suma);
+
+suma = 0;
+while(suma < 3){
+    suma=suma + 1;
     console.log(suma);
 }
