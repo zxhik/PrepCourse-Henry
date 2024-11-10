98218a2649689b88533df65e57c899a3c2db401b
Zxhik <zxhik000@gmail.com>
2024-11-10 20:52:38 -0300

    Patch 1.7

---
+++ arrays.js
@@ -0,0 +1,9 @@
+var listaDeCompra = [];
+listaDeCompra [3] = "Tomate";
+listaDeCompra [1] = "Lechuga"
+console.log (listaDeCompra);
+console.log (listaDeCompra.length);
+
+
+var elementoDelArray = listaDeCompra [1];
+console.log (elementoDelArray);
